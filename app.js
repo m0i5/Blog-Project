824c2ff0eab88f9bf8d072ac250a70850450b1a7
m0i5 <daneidenbenz@gmail.com>
2023-02-02 19:41:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 
+
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}));
