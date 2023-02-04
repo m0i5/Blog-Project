6d9996de3923ca2e418beae4904fde2b54d5ccf6
m0i5 <daneidenbenz@gmail.com>
2023-02-04 18:57:03 +0100

    Final
    - routing with express.js
    - create post.ejs where the single blog posts are displayed
    - Used substring for the preview of posts
    - linked it to /posts/<%= post.Title %>

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require('lodash');
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -51,7 +52,6 @@ app.get("/compose", function(req, res) {
   });
 });
 
-
 app.post("/compose", function(req, res) {
   const post = {
     Title: req.body.postTitle,
@@ -60,14 +60,46 @@ app.post("/compose", function(req, res) {
 
 
 
-
-posts.push(post.Title);
+  posts.push(post);
   res.redirect("/");
 
 
 });
 
 
+
+
+// app.get("/posts/:postName", function(req, res) {
+// const requestedTitle = req.params.postName
+//
+// posts.forEach((post)=>{
+//   const storedTitle = post.Title;
+// });
+//
+// if (storedTitle === requestedTitle) {
+//   console.log('Match found')
+// }
+// });
+
+app.get("/posts/:postName", function(req, res) {
+
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.Title);
+
+    if (requestedTitle === storedTitle) {
+
+      res.render("post", {
+        title: post.Title,
+        text: post.Text
+
+      });
+
+    }
+  })
+});
 
 
 
